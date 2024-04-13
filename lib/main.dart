fd5894d437bdf97322c1015190e2c7c88ad6b516
macaulay famous <macaulayfamous@gmail.com>
2024-04-13 21:38:42 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:mac_store_app/controllers/category_controller.dart';
+import 'package:mac_store_app/views/screens/main_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+            apiKey: "AIzaSyDK_N9UIzaiu2FjlP5i-E4dMqMxGjKZ5BY",
+            appId: '1:69723042368:android:350bea0d69d405dcfb65a5',
+            messagingSenderId: '69723042368',
+            projectId: 'shop-a84e5',
+            storageBucket: "shop-a84e5.appspot.com",
+          ),
+        )
+      : await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MainScreen(),
+      initialBinding: BindingsBuilder(
+        () {
+          Get.put<CategoryController>(CategoryController());
+        },
+      ),
+    );
+  }
+}
---
+++ lib/views/screens/nav_screens/widgets/recommended_product_widget.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:mac_store_app/views/screens/nav_screens/widgets/product_item_widget.dart';
+
+class RecommenedProductWidget extends StatelessWidget {
+  const RecommenedProductWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Stream<QuerySnapshot> productsStream =
+        FirebaseFirestore.instance.collection('products').snapshots();
+
+    return StreamBuilder<QuerySnapshot>(
+      stream: productsStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return const Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        return SizedBox(
+          height: 250,
+          child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                final productData = snapshot.data!.docs[index];
+                return ProductItemWidget(productData: productData,);
+              }),
+        );
+      },
+    );
+  }
+}
